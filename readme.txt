ef6f61b2a23346146fcb4bda95384023a8b3e79b
flyplugins <flyplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-14 04:44:39 +0000

    Adding 1.1 to trunk
    git-svn-id: https://plugins.svn.wordpress.org/premise-addon-for-wp-courseware/trunk@787379 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,80 @@
+=== Premise Addon for WP Courseware ===
+Contributors: flyplugins
+Donate link: http://flyplugins.com/donate
+Tags: fly plugins,Premise for WordPress,wp courseware,wpcourseware,lms,lms for wordpress,learning management system,online ecourse,ecourse,flyplugins,wordpress learning management system
+Requires at least: 3.0
+Tested up to: 3.6.1
+Stable tag: 1.1
+
+This plugin adds integration between Premise and WP Courseware which allows you to assign course(s) to products for auto enrollment.
+
+== Description ==
+[Fly Plugins](http://flyplugins.com) presents [Premise for WordPress](http://flyplugins.com/premise-for-wordpress) for [WP Courseware](http://www.wpcourseware.com).
+
+= Have you ever wanted to sell an online internet course or ecourse? =
+The Premise for WordPress Addon for WP Courseware will add full integration with WP Courseware. It will allow you to assign WP Courseware courses to a membership level. Once the course(s) are assigned to a membership level, upon purchase of a membership level and registration, a student will automatically be enrolled into the associated courses.
+
+With this addon, you will be able to create a fully automated LMS system which you can sell online, or if you choose to run a free online course, you can do so with ease.
+
+= Premise Plugin Integration with WP Courseware Plugin =
+[youtube http://www.youtube.com/watch?v=bZag-Z2kZus]
+
+= Basic Configuration Steps =
+1. Create a course with WP Courseware and add module(s), unit(s), and quiz(zes)
+2. Add units to modules, and add quizzes to units with in modules
+3. Create a course outline page using [shortcode]
+4. Create a product and set a price
+5. Associate one or more WP Courseware courses with the membership level
+6. New users pay for the course through the shopping cart integration of your choice, the membership plugin assigns them WP user access and assigns them to the membership level for which they paid, and WP Courseware assigns them to the appropriate course(s) based on the membership level
+
+= Check out Fly Plugins =
+For more info about Fly Plugins Free and Premium Plugins, check out the following links:
+
+* [WP Courseware LMS plugin for WordPress](http://flyplugins.com/wp-courseware-premium-wordpress-plugin/) - The best LMS online for WordPress.
+* [S3 Media Maestro](http://www.s3mediamaestro.com/) - The best HTML 5 secure media player for WordPress that works hand in hand with Amazon S3.
+* Other [Fly PluginsWordPress Plugins](http://flyplugins.com) by Fly Plugins.
+* Follow Fly Plugins on [Facebook](http://flyplugins.com/facebook) 
+* Check out the Fly Plugins [YouTube](http://www.youtube.com/flyplugins) channel.
+* See documentation on our [Codex](http://flyplugins.com/codex) site.
+
+== Installation ==
+
+1. Upload the `Premise for WordPress for WP Courseware addon` folder into the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Configure the plugin by going into Training Courses-->Premise for WordPress, then associate courses with membership levels (this assumes you already have courses and membership levels created).
+
+== Frequently asked questions ==
+
+= Does this plugin require WP Courseware to already be installed =
+
+Yes!
+
+= Does this plugin require Premise for WordPress to already be installed =
+
+Yes!
+
+= Where can I get WP Courseware? =
+
+Click here to get the [Best WordPress LMS Plugin](http://www.wpcourseware.com).
+
+= Where can I get Premise for WordPress? =
+
+Click here to get [Premise for WordPress](http://flyplugins.com/premise-for-wordpress).
+
+== Screenshots ==
+
+1. The Course Access Settings screen will display which products are associated with which courses
+
+2. This is the actual configuration screen where you can select courses that will be associated with a particular membership level
+
+== Changelog ==
+
+= 1.1 =
+* Fixed bug that prevented automatic enrollment
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade notice ==
+
---
+++ wp-courseware-premise.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * Plugin Name: WP Courseware - Premise Membership Add On
+ * Version: 1.1
+ * Plugin URI: http://flyplugins.com
+ * Description: The official extension for WP Courseware to add support for the Premise Membership membership plugin for WordPress.
+ * Author: Fly Plugins
+ * Author URI: http://flyplugins.com
+ */
+/*
+ Copyright 2013 Fly Plugins
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+
+// Main parent class
+include_once 'class_members.inc.php';
+
+
+// Hook to load the class
+add_action('init', 'WPCW_Premise_init',1);
+
+/**
+ * Initialise the membership plugin, only loaded if WP Courseware 
+ * exists and is loading correctly.
+ */
+function WPCW_Premise_init()
+{
+	$item = new WPCW_Premise();
+	
+	// Check for WP Courseware
+	if (!$item->found_wpcourseware()) {
+		$item->attach_showWPCWNotDetectedMessage();
+		return;
+	}
+	
+	// Not found the membership tool
+	if (!$item->found_membershipTool()) {
+		$item->attach_showToolNotDetectedMessage();
+		return;
+	}
+	
+	// Found the tool and WP Courseware, attach.
+	$item->attachToTools();
+}
+
+
+/**
+ * Membership class that handles the specifics of the Premise Membership WordPress plugin and
+ * handling the data for levels for that plugin.
+ */
+class WPCW_Premise extends WPCW_Members
+{
+	const GLUE_VERSION  = 1.00; 
+	const EXTENSION_NAME = 'Premise Membership';
+	const EXTENSION_ID = 'WPCW_Premise';
+	
+	/**
+	 * Main constructor for this class.
+	 */
+	function __construct()
+	{
+		// Initialise using the parent constructor 
+		parent::__construct(WPCW_Premise::EXTENSION_NAME, WPCW_Premise::EXTENSION_ID, WPCW_Premise::GLUE_VERSION);
+	}
+	
+	
+	/**
+	 * Get the membership levels for this specific membership plugin.
+	 */
+	protected function getMembershipLevels()
+	{
+	$args=array(
+  		'post_type' => 'acp-products',
+  		'post_status' => 'publish',
+  		'numberposts' => -1
+	);
+	
+	$levelData = get_posts($args);
+	
+		if ($levelData && count($levelData) > 0)
+		{
+			$levelDataStructured = array();
+
+			// Format the data in a way that we expect and can process
+			foreach ($levelData as $levelDatum)
+			{
+				$levelItem = array();
+				$levelItem['name'] 	= $levelDatum->post_title;
+				$levelItem['id'] 	= $levelDatum->ID;
+				$levelItem['raw'] 	= $levelDatum;
+
+				$levelDataStructured[$levelItem['id']] = $levelItem;
+			}
+			return $levelDataStructured;
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * Function called to attach hooks for handling when a user is updated or created.
+	 */	
+	
+	protected function attach_updateUserCourseAccess()
+	{
+		// Events called whenever the user products are changed, which updates the user access.
+		add_action('premise_checkout_complete_after', 		array($this, 'handle_newUserCourseAccess'),10,3);
+		add_action('memberaccess_edit_order', 		array($this, 'handle_updateUserCourseAccess'),10,3);
+
+
+	}
+
+	/**
+	 * Function just for handling the membership callback, to interpret the parameters
+	 * for the class to take over.
+	 * 
+	 * @param Integer $user is the ID if the user being created.
+	 * @param Array $membership_level is level assigned to user.
+	 */
+	public function handle_newUserCourseAccess( $checkout_args, $product_id, $args)
+	{
+		// Get user ID from transaction
+		$user_info = wp_get_current_user();
+        $user = $user_info->ID;
+        //Returns product the user has purchased and is paid up on.
+		$membership_level = $product_id;
+		// Over to the parent class to handle the sync of data.
+		parent::handle_courseSync($user, array($membership_level));
+	}
+
+	public function handle_updateUserCourseAccess( $post, $values, $old_values)
+	{
+		// Get user ID from change in order status
+		$user = $old_values['_acp_order_member_id'] ;
+        //Returns product the user has changed to.
+		$membership_level = $values['_acp_order_product_id'];
+		// Over to the parent class to handle the sync of data.
+		parent::handle_courseSync($user, array($membership_level));
+	}
+		
+	
+	/**
+	 * Detect presence of the membership module.
+	 */
+	public function found_membershipTool()
+	{
+	     return function_exists( 'memberaccess_init' );
+	}
+}
+?>
